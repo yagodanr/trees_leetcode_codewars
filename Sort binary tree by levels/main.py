87878ed6f8faa818dbd5fe19f2a0fe5c8815d5bf
yagodanr <yagodanr@gmail.com>
2025-04-16 09:07:57 +0300

    Sort Binary tree by levels

--- Sort binary tree by levels/main.py
+++ Sort binary tree by levels/main.py
@@ -1,3 +1,6 @@
+from queue import Queue
+
+
 class Node:
     def __init__(self, L, R, n):
         self.left = L
@@ -7,4 +10,17 @@ class Node:
 
 
 def tree_by_levels(node: Node) -> list:
-    return
+    l = []
+
+    s =  Queue()
+    s.put(node)
+    while not s.empty():
+        cur = s.get()
+        if cur is None:
+            continue
+
+        s.put(cur.left)
+        s.put(cur.right)
+        l.append(cur.value)
+
+    return l
