76e569fe72dad12e9645d01f478e93763cf78553
yagodanr <yagodanr@gmail.com>
2025-04-16 08:59:28 +0300

    Binary Tree Traversal

--- Binary Tree Traversal/main.py
+++ Binary Tree Traversal/main.py
@@ -14,9 +14,13 @@ def pre_order(node: Node) -> list:
 
 # In-order traversal
 def in_order(node: Node) -> list:
+    if node is None:
+        return []
     return in_order(node.left) + [node.data] + in_order(node.right)
 
 # Post-order traversal
 def post_order(node: Node) -> list:
-    return []
+    if node is None:
+        return []
+    return post_order(node.left) + post_order(node.right) + [node.data]
 
